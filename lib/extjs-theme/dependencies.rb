19209ba60abee0ea2c1705ff29a8f123d04b75bd
David A. Cuadrado <krawek@gmail.com>
2010-02-13 10:44:34 +0800

    fix dependencies
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/extjs-theme/dependencies.rb
+++ lib/extjs-theme/dependencies.rb
@@ -1,16 +1,18 @@
 begin
   gem 'haml-edge', '>= 2.3.0'
   $stderr.puts "Loading haml-edge gem."
-  
+rescue LoadError
+  #pass
+end
+
+begin
   gem 'rmagick'
   $stderr.puts "Loading rmagick gem."
   
-rescue Exception
+rescue LoadError
   #pass
 end
 
 require 'sass'
-require 'rmagick'
-
-
+require 'RMagick'
 
